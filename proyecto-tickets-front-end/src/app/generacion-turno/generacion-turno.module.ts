c3d5bf5f0fd71dc1f960b18882176ca2e98eb19a
José M. Martínez García <jmlmartinezg@outlook.com>
2019-06-16 16:00:20 -0400

    bootstrap-install

---
+++ proyecto-tickets-front-end/src/app/generacion-turno/generacion-turno.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TipoTurnoBtnComponent } from './tipo-turno-btn/tipo-turno-btn.component';
+
+@NgModule({
+  declarations: [TipoTurnoBtnComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class GeneracionTurnoModule { }
---
+++ proyecto-tickets-front-end/src/app/generacion-turno/tipo-turno-btn/tipo-turno-btn.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tipo-turno-btn',
+  templateUrl: './tipo-turno-btn.component.html',
+  styleUrls: ['./tipo-turno-btn.component.css']
+})
+export class TipoTurnoBtnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
