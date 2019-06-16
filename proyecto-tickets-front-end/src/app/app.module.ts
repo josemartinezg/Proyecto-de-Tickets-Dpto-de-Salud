e5160af610030642794242c6ed107ac1508ea406
José M. Martínez García <jmlmartinezg@outlook.com>
2019-06-16 18:17:20 -0400

    api-posts-1

--- proyecto-tickets-front-end/src/app/app.module.ts
+++ proyecto-tickets-front-end/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 
 
 import { GeneracionTurnoModule } from './generacion-turno/generacion-turno.module';
+import { HttpClient } from 'selenium-webdriver/http';
 
 @NgModule({
   declarations: [
@@ -14,8 +15,10 @@ import { GeneracionTurnoModule } from './generacion-turno/generacion-turno.modul
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule, 
-    GeneracionTurnoModule
+    GeneracionTurnoModule,
+    
   ],
   providers: [
 
