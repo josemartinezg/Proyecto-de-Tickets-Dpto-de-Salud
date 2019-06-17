5cee22bee756132b180245daacb188da592b9149
José M. Martínez García <jmlmartinezg@outlook.com>
2019-06-17 14:26:00 -0400

    more-button-styles

--- test-app/src/app/components/tipo-turno-btn/tipo-turno-btn.component.ts
+++ test-app/src/app/components/tipo-turno-btn/tipo-turno-btn.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
 import { TipoTurnoBtnService } from './tipo-turno-btn.service';
+import { HttpClient } from '@angular/common/http';
 import { createAotUrlResolver } from '@angular/compiler';
 
 @Component({
@@ -10,18 +11,44 @@ import { createAotUrlResolver } from '@angular/compiler';
 export class TipoTurnoBtnComponent implements OnInit {
   turnos;
   @Input('is-active') isActive: boolean;
-  generarTurno(turno){
-    var today = new Date();
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    var time = today.getHours()+":"+today.getMinutes()+":"+today.getSeconds();
-    var dateTime = date+' '+time;
-    console.log(dateTime + ' ' + turno);
-  }
-  constructor(service: TipoTurnoBtnService) { 
+  response: any;
+  url = 'http://localhost:3000/api/turnos';
+  constructor(service: TipoTurnoBtnService, private http: HttpClient) { 
     /*Cuando se le pone este parámetro al constructor se desacopla,
      el servicio del componente, que es lo que queremos. */
     this.turnos = service.getTipoTurno(); 
    }
+
+
+  generarTurno(turno){
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours()+":"+today.getMinutes()+":"+today.getSeconds();
+    var dateTime = date+'T'+time;
+    console.log(dateTime + ' ' + turno);
+
+    let post = {
+      tipo_turno: turno,
+      tipo_consulta: "estudiante",
+      id_paciente: "20150379",
+      id_empleado_salud: "corodriguez",
+      codigo_turno: "string",
+      fecha_hora_emision: "2019-06-12T06:03:28.779Z",
+      fecha_hora_inicio: "2019-06-12T06:03:28.779Z",
+      fecha_hora_fin: "2019-06-12T06:03:28.779Z"
+    }
+    this.http.get(this.url)
+    .subscribe((response) => {
+        this.response = response;
+        console.log(response);
+      })
+    this.http.post(this.url, post)
+    .subscribe(response =>{
+    console.log(response);
+    })
+    console.log("Generar turno.");
+  }
+
 
   ngOnInit() {
   }
