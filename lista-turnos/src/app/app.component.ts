91df07abdc6108c64c8a97ef795063eb0356fbbe
Nax17 <33287649+Nax17@users.noreply.github.com>
2019-06-23 19:31:01 -0400

    Intentando mostrar turnos en una lista

--- lista-turnos/src/app/app.component.ts
+++ lista-turnos/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { OnInit, Component } from '@angular/core';
+import { ITurno } from './turnos/turnos.model';
+import { TurnoService } from './turnos/turno.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  providers:[TurnoService]
 })
-export class AppComponent {
-  title = 'lista-turnos';
+export class AppComponent implements OnInit{
+  turnos : ITurno[];
+  selectedTurno : ITurno;
+
+  constructor(private turnoService:TurnoService){}
+
+  ngOnInit(): void {
+    this.getTurno();
+  }
+
+  getTurno():void{
+    this.turnoService.getTurno().then(turno => this.turnos = turno);
+  }
+
+  onSelect(turno:ITurno):void{
+    this.selectedTurno = turno;
+  }
 }
---
+++ lista-turnos/src/app/turnos/mock-turns.ts
@@ -0,0 +1,7 @@
+import { ITurno } from './turnos.model';
+
+export const Turnos : ITurno[] = [
+    {id:1, name:"Luis"},
+    {id:2, name:"Oliver"},
+    {id:3, name:"Josema"}
+];
