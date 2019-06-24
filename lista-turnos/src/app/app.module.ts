6041fbde717b4744dccf1eef89edfa33666e516a
Nax17 <33287649+Nax17@users.noreply.github.com>
2019-06-23 20:15:25 -0400

    API Turnos

--- lista-turnos/src/app/app.module.ts
+++ lista-turnos/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TurnoDetailComponent} from './turnos/turnos-detail.component';
+import { FormsModule } from '@angular/forms';
+import {MatListModule} from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar'
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { TurnoDetailComponent} from './turnos/turnos-detail.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatListModule, 
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- lista-turnos/src/app/turnos/mock-turns.ts
+++ lista-turnos/src/app/turnos/mock-turns.ts
@@ -3,5 +3,6 @@ import { ITurno } from './turnos.model';
 export const Turnos : ITurno[] = [
     {id:1, name:"Luis"},
     {id:2, name:"Oliver"},
-    {id:3, name:"Josema"}
+    {id:3, name:"Josema"},
+    {id:4, name:"Alonso"}
 ];
