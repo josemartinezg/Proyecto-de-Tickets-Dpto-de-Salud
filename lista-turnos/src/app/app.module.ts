727587eb5f07f4afff24de0c5da3d554a17646c0
José M. Martínez García <jmlmartinezg@outlook.com>
2019-06-30 23:21:52 -0400

    rough-list-pacientes

--- lista-turnos/src/app/app.module.ts
+++ lista-turnos/src/app/app.module.ts
@@ -7,17 +7,30 @@ import { TurnoDetailComponent} from './turnos/turnos-detail.component';
 import { FormsModule } from '@angular/forms';
 import {MatListModule} from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule }from '@angular/material/table';
 import { TurnoTxtComponent } from './turno-txt/turno-txt.component';
 import { TurnoComponent } from './turno/turno.component'
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { Turno } from './models/Turno';
+import { TurnosUsuarioAtencionComponent } from './turnos-usuario-atencion/turnos-usuario-atencion.component';
+import { MatPaginatorModule, MatSortModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     TurnoDetailComponent,
     TurnoTxtComponent,
-    TurnoComponent
+    TurnoComponent,
+    TurnosUsuarioAtencionComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +39,16 @@ import { RouterModule } from '@angular/router';
     MatListModule, 
     MatToolbarModule,
     HttpClientModule,
-    RouterModule.forRoot()
+    MatTableModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent},
+      { path: 'usuario-atencion', component: TurnosUsuarioAtencionComponent},
+      
+    ]),
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
