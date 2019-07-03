bb205e94f2c64f18623a2f2cec72f702e945cb7d
José M. Martínez García <jmlmartinezg@outlook.com>
2019-07-03 19:20:24 -0400

    sockets-changes

--- proyecto-tickets/server/boot/automigrate.js
+++ proyecto-tickets/server/boot/automigrate.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function(app){
-    app.dataSources.postgre1.autoupdate(['turno', 'consulta', 'usuario_atencion', 'estado'], err =>{
+    app.dataSources.postgre1.autoupdate(['turno', 'consulta', 'usuario_atencion', 'estado', 'numero_puesto'], err =>{
         if (err) throw err;
         console.log('Models Synced');
     })
