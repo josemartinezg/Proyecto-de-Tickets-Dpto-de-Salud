e0a5002acb7a6ff07a6d90f86d48635f70aba766
José M. Martínez García <jmlmartinezg@outlook.com>
2019-08-12 12:55:45 -0400

    updating metabase credentials

--- test-app/src/app/admin/dashboard/dashboard.component.ts
+++ test-app/src/app/admin/dashboard/dashboard.component.ts
@@ -23,13 +23,13 @@ export class DashboardComponent implements OnInit {
   }
 
    getDashboard(){
-    const METABASE_SITE_URL = "http://localhost:3000";
-    const METABASE_SECRET_KEY = "7408c543ef16470cbf2ffdf126aa73347d622e3d05ec8d2d65d40ed41537be95";
+    const METABASE_SITE_URL = "http://192.168.77.9:3000";
+    const METABASE_SECRET_KEY = "fbff2ef3a2182c1c580f98c11a7d47ca1e55fb1043dc861cd066bb363b6a3d4e";
 
      const payload = {
       resource: { dashboard: 1 },
       params: {},
-      //exp: Math.round(Date.now() / 1000) + (10 * 60) // 10 minute expiration
+      exp: Math.round(Date.now() / 1000) + (10 * 60) // 10 minute expiration
     };
     const token = jwt.sign(payload, METABASE_SECRET_KEY);
 
