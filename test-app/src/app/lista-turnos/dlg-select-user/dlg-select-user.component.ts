216487118b99be0582cd4b1f9f28636a79f3ab42
Nax17 <33287649+Nax17@users.noreply.github.com>
2019-08-23 17:20:54 -0400

    Sincronia Resuelta

--- test-app/src/app/lista-turnos/dlg-select-user/dlg-select-user.component.ts
+++ test-app/src/app/lista-turnos/dlg-select-user/dlg-select-user.component.ts
@@ -126,15 +126,21 @@ export class DlgSelectUserComponent implements OnInit {
     }
 
     onOtherClick(){
-      this.turnoService.getTurnosEstado1().subscribe(misTurnos1 => {this.misTurnos1 = misTurnos1
-      console.log(misTurnos1)});
-      this.turnoService.getTurnosEstado2().subscribe(misTurnos2 => {this.misTurnos2 = misTurnos2
-      console.log(misTurnos2)}); 
-      setTimeout(this.fuckThisShit, 2000);
+      const TmisTurnos1 = this.turnoService.getTurnosEstado1().subscribe(misTurnos1 => {this.misTurnos1 = misTurnos1
+      console.log(misTurnos1)
+      misTurnos1.reduce((a,obj) => a + Object.keys(obj).length, 0)});
+      const TmisTurnos2 = this.turnoService.getTurnosEstado1().subscribe(misTurnos2 => {this.misTurnos2 = misTurnos2
+      console.log(misTurnos2)
+       misTurnos2.reduce((a,obj) => a + Object.keys(obj).length, 0)});
+      //const TmisTurnos1 = this.misTurnos1.reduce((a, obj) => a + Object.keys(obj).length, 0);
+      //const TmisTurnos2 = this.misTurnos2.reduce((a, obj) => a + Object.keys(obj).length, 0);
+      //setTimeout(, 2000);
+      this.fuckThisShit(TmisTurnos1, TmisTurnos2);
     }
     
-    fuckThisShit(){
-      if(this.misTurnos1.length < this.misTurnos2.length){
+    fuckThisShit(TmisTurnos1: any, TmisTurnos2: any){
+      
+      if(TmisTurnos1 < TmisTurnos2){
         this.turnoService.createTurno(this.data.turno, "1");
       }else{
         this.turnoService.createTurno(this.data.turno, "2");
